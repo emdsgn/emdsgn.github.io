02146ec783d5024440e01999e0e451ed6a289679
Mansura Chowdhury <c.mansura@gmail.com>
2016-10-11 21:14:15 -0400

    sad

--- _site/js/contactform.js
+++ _site/js/contactform.js
@@ -56,22 +56,3 @@ if (name == "") {
     }
   });
   return false;
-
-   var name = $("input#name").val();
-   var dataString = 'name='+ name + '&email=' + email + '&phone=' + phone;
-
- $.ajax({
-      type: "POST",
-      url: "bin/process.php",
-      data: dataString,
-      success: function() {
-        $('#contact_form').html("<div id='message'></div>");
-        $('#message').html("<h2>Contact Form Submitted!</h2>")
-        .append("<p>We will be in touch soon.</p>")
-        .hide()
-        .fadeIn(1500, function() {
-          $('#message').append("<img id='checkmark' src='images/check.png' />");
-        });
-      }
-    });
-    return false;
