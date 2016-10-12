05e56c0aa8a16c20dcfcbde6170e3060f5aaccd9
Mansura Chowdhury <c.mansura@gmail.com>
2016-10-11 21:17:46 -0400

    sa

--- _site/js/contactform.js
+++ _site/js/contactform.js
@@ -1,58 +1,71 @@
- $(function() {
-    $(".button").click(function() {
-      // validate and process form here
+<!-- import jQuery library -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+  var form = $('#form'); // contact form
+  var submit = $('#submit');  // submit button
+  var alert = $('.alert'); // alert div for show alert message
+
+  // form submit event
+  form.on('submit', function(e) {
+    e.preventDefault(); // prevent default form submit
+
+    $.ajax({
+      url: '', // form action url
+      type: 'POST', // form submit method get/post
+      dataType: 'html', // request type html/json/xml
+      data: form.serialize(), // serialize form data 
+      beforeSend: function() {
+        alert.fadeOut();
+        submit.html('Sending....'); // change submit button text
+      },
+      success: function(data) {
+        alert.html(data).fadeIn(); // fade in response data
+        form.trigger('reset'); // reset form
+        submit.html('Send Email'); // reset submit button text
+      },
+      error: function(e) {
+        console.log(e)
+      }
     });
   });
+});
 
-  $(function() {
-    $('.error').hide();
-    $(".button").click(function() {
-      // validate and process form here
-      
-      $('.error').hide();
-  	  var name = $("input#name").val();
-  		if (name == "") {
-        $("label#name_error").show();
-        $("input#name").focus();
-        return false;
-      }
-  		var email = $("input#email").val();
-  		if (email == "") {
-        $("label#email_error").show();
-        $("input#email").focus();
-        return false;
-      }
-  		var phone = $("input#phone").val();
-  		if (phone == "") {
-        $("label#phone_error").show();
-        $("input#phone").focus();
-        return false;
-      }
-      
-    });
-  });
+# if request sent using HTTP_X_REQUESTED_WITH
+if( isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) ){
+  if (isset($_POST['name']) AND isset($_POST['email']) AND isset($_POST['subject']) AND isset($_POST['message'])) {
+    $to = 'your@mail.id';
 
-  var name = $("input#name").val();
-if (name == "") {
-    $("label#name_error").show();
-    $("input#name").focus();
-    return false;
-  }
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+    $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 
-  var dataString = 'name='+ name + '&email=' + email + '&phone=' + phone;
-  //alert (dataString);return false;
-  $.ajax({
-    type: "POST",
-    url: "bin/process.php",
-    data: dataString,
-    success: function() {
-      $('#contact_form').html("<div id='message'></div>");
-      $('#message').html("<h2>Contact Form Submitted!</h2>")
-      .append("<p>We will be in touch soon.</p>")
-      .hide()
-      .fadeIn(1500, function() {
-        $('#message').append("<img id='checkmark' src='images/check.png' />");
-      });
+    $sent = email($to, $email, $name, $subject, $message);
+    if ($sent) {
+      echo 'Message sent!';
+    } else {
+      echo 'Message couldn\'t sent!';
     }
-  });
-  return false;
+  }
+  else {
+    echo 'All Fields are required';
+  }
+  return;
+}
+
+/**
+ * Email send with headers
+ *
+ * @return bool | void
+ **/
+function email($to, $from_mail, $from_name, $subject, $message){
+  $header = array();
+  $header[] = "MIME-Version: 1.0";
+  $header[] = "From: {$from_name}<{$from_mail}>";
+  /* Set message content type HTML*/
+  $header[] = "Content-type:text/html; charset=iso-8859-1";
+  $header[] = "Content-Transfer-Encoding: 7bit";
+  if( mail($to, $subject, $message, implode("\r\n", $header)) ) return true; 
+}
+</script>
